a94193871f4ddf6a713309f4d6a0cfe48f437bb3
samay910 <samaychadha.sc@gmail.com>
2025-05-05 15:36:10 +0100

    Small updates made to constants

--- Network/src/commonMain/kotlin/com/samay910/networking/Constants.kt
+++ Network/src/commonMain/kotlin/com/samay910/networking/Constants.kt
@@ -2,9 +2,10 @@ package com.samay910.networking
 
 //here is where the api endpoints and keys are stored
 object Constants {
+    // Your gemeini API key will go here
     val geminiApiKey ="AIzaSyBB2impdjPfHQ0XS5HqhGsoqpJg4wJG0oA"
+    // Your newsapi.org key will go here
     val newsApiKey="598d979561414adfb32916550d4d8791"
     val geminiURL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
     val newsapiURL="https://newsapi.org/v2/everything"
-
 }
